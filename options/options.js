dba2b7c4825338dfe28c4dfd382e460833cf4258
Crowdbotics <team@crowdbotics.com>
2023-05-09 13:28:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-cell-40578",
+  url: "https://cool-cell-40578.botics.co",
+  api: "https://cool-cell-40578.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
